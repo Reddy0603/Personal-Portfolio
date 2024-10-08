8991aa1cb01353fedb0c6fe6aebf6f793ede7dfa
Reddy0603 <ashokashokm2001@gmail.com>
2024-10-08 18:49:53 +0530

    change in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import PortfolioContainer from './PortfolioContainer/PortfolioContainer';
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css'
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <ToastContainer/>
-    <PortfolioContainer/>
+      <ToastContainer />
+      <h1>Hey hello</h1>
+      <PortfolioContainer />
     </div>
   );
 }
